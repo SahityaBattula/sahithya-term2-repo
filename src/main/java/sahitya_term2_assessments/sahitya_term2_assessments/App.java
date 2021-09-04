e96ef1554707f0340b87562e48c7e89034601fd6
unknown <Sahitya>
2021-09-04 17:04:49 +0530

    commited

---
+++ src/main/java/sahitya_term2_assessments/sahitya_term2_assessments/App.java
@@ -0,0 +1,13 @@
+package sahitya_term2_assessments.sahitya_term2_assessments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome sahitya to the Term 2 Assessment" );
+    }
+}
---
+++ src/test/java/sahitya_term2_assessments/sahitya_term2_assessments/AppTest.java
@@ -0,0 +1,20 @@
+package sahitya_term2_assessments.sahitya_term2_assessments;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
